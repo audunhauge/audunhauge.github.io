0eaef3446a2552d0b4bf09fc1ee163d1eefa5e3e
audunhauge <audun@SE02236.local>
2017-11-28 09:21:01 +0100

    minor

--- it1/dyrlege/kundeskjema.js
+++ it1/dyrlege/kundeskjema.js
@@ -7,7 +7,7 @@ function setup() {
         projectId: "civz-7871d",
         storageBucket: "civz-7871d.appspot.com",
         messagingSenderId: "324948067446"
-      };
+    };
     firebase.initializeApp(config);
     let spanKunde = document.getElementById("kundevelger");
     let divDyr = document.getElementById("dyr");
@@ -19,13 +19,32 @@ function setup() {
         if (kundene) {
             let dropBox = makeDrop(kundene);
             spanKunde.innerHTML = dropBox;
+
+            let drpKunde = document.getElementById("kundenr");
+            drpKunde.addEventListener("change", visDyr);
         }
     });
+
+    function visDyr(e) {
+        let ref = firebase.database().ref("dyr");
+        ref.once("value").then(function (snapshot) {
+            let dyrene = snapshot.val();
+            if (dyrene) {
+                let dyrnr = Object.keys(dyrene);
+                let dyrliste = `<ol>` +
+                    dyrnr.map(e => `<li>${dyrene[e].navn} ${dyrene[e].art}</li>`).join("")
+                    + `</ol>`;
+                divDyr.innerHTML = dyrliste;
+
+            }
+        });
+    }
+
 
     function makeDrop(kunder) {
         let box = '<select id="kundenr">';
         let kundenr = Object.keys(kunder);
-        let navn = kundenr.map(e => 
+        let navn = kundenr.map(e =>
             `<option values="${e}">${kunder[e].fornavn}</option>`);
         box += navn.join("") + "</select>";
         return box;
