278488a270b447fa29ad439e4577d68babb243ea
audunhauge <audun@SE02236.local>
2017-12-12 09:00:43 +0100

    drop pakker

---
+++ it1/Julekort/julekort.js
@@ -0,0 +1,18 @@
+function setup() {
+    let divSky = document.getElementById("sky");
+    let divSanta = document.getElementById("santa");
+    let stars = divSky.querySelectorAll(".star");
+    stars.forEach(stjerne => {
+        stjerne.style.left = -120 + Math.floor(Math.random() * 240) + "px";
+        stjerne.style.top = -80 + Math.floor(Math.random() * 160) + "px";
+    });
+
+    divSanta.addEventListener("click" , dropGifts);
+
+    function dropGifts(e) {
+        let pakke = document.createElement('div');
+        pakke.className = "pakke";
+        pakke.style.left = e.screenX + "px";
+        pakke.style.top = e.screenX + "px";
+    }
+}
