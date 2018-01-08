09fcb1d40b4416773c7ae6d34bd591e236a9b568
audun hauge <audun.hauge@gmail.com>
2018-01-08 22:57:37 +0100

    løsningsforslag

--- it2/oppsum/oppsum.js
+++ it2/oppsum/oppsum.js
@@ -9,14 +9,62 @@ function setup() {
    /* div med små bilder */
    let divEx = document.getElementById("ex");
 
-   /* min-apper for hver beskrevet oppgave */
-   let divStorPeter = document.getElementById("storpeter");
-   let divStorNuYok = document.getElementById("nuyok");
-   let divStorRoma = document.getElementById("roma");
+/******* mini-apper for hver beskrevet oppgave *********/
 
-    /*  vise stort bilde av petersburg */
+   /**  vis stor peter */
+   let divStorPeter = document.getElementById("storpeter");
+
+   /** vis quiz om newyork */
+   let divStorNuYok = document.getElementById("nuyok");
+   let btnNewTilbake = divStorNuYok.querySelector("button");
+   
+   
+   /** spill av video om roma */
+   let divStorRoma = document.getElementById("roma");
+   let btnRomaTilbake = divStorRoma.querySelector("button");
+
+
+
+
+
+/********  quiz om bilde av new york ***********/
+    divNuYok.addEventListener("click", quiz);
+    btnNewTilbake.addEventListener("click", visLilleYork);
+    
+    function quiz(e) {
+        divEx.classList.add("hidden");
+        divStorNuYok.classList.remove("hidden");
+    }
+
+    function visLilleYork(e) {
+        divEx.classList.remove("hidden");
+        divStorNuYok .classList.add("hidden");
+     }
+
+    let selQuiz = divStorNuYok.querySelector("select");
+    selQuiz.addEventListener("change", sjekk);
+
+    function sjekk(e) {
+        let v = selQuiz.value;
+        let melding = "";
+        if (v === "1") {
+            melding = "Riktig";
+        } else {
+            if (v === "0") {
+                melding = "Galt";
+            }
+        }
+        divStorNuYok.querySelector("p").innerHTML = melding;
+    }
+
+
+
+
+/*************  vise stort bilde av petersburg ************/
     divPeter.addEventListener("click", visStorPeter);
     divStorPeter.addEventListener("click", visLillePeter);
+    // ikke egen knapp - da det ikke finnes andre interaktive elementer
+    // før eller siden klikker alle på skjermen ....
 
     function visStorPeter(e) {
        divEx.classList.add("hidden");
@@ -28,9 +76,12 @@ function setup() {
        divStorPeter.classList.add("hidden");
     }
 
-    /* vise video av roma */
+
+
+
+/************* vise video av roma ********** */
     divRoma.addEventListener("click", visFilm);
-    divStorRoma.addEventListener("click", visLilleRoma);
+    btnRomaTilbake.addEventListener("click", visLilleRoma);
 
     function visFilm(e) {
        divEx.classList.add("hidden");
