bf19669733372e0afc35229ff71e8ba64bfe01c7
audunhauge <audun@SE02236.local>
2018-01-16 09:00:23 +0100

    firebase rediger database

--- it2/oppsum/reise.js
+++ it2/oppsum/reise.js
@@ -4,9 +4,9 @@ function setup() {
 
     let prisliste = {
         newyork: {
-            Sheraton: [ 2000, 2500],
-            Raddison: [ 2200, 2300],
-            BigBlue: [ 1200, 1500],
+            Sheraton: [2000, 2500],
+            Raddison: [2200, 2300],
+            BigBlue: [1200, 1500],
         }
     }
     // enkeltrom = prisliste["newyork"]["Sheraton"][0]
@@ -25,15 +25,61 @@ function setup() {
         let by = selBy.value;
         divByInfo.innerHTML = "";
         if (hoteller[by]) {
-          let byHotell = hoteller[by];
-          let selHotell = document.createElement('select');
-          let s = "";
-          for (let hotell of byHotell) {
-              s += `<option>${hotell}</option>`;
-          }
-          selHotell.innerHTML = s;
-          divByInfo.appendChild(selHotell);
+            bestilling(by)
+        } else {
+            divByInfo.innerHTML = "Ingen info ennå ... ";
         }
-       
+
+    }
+
+    function bestilling(by) {
+        let enkelt, dobbelt;  // husk prisen for rom - endres ved valg av hotell
+        let byHotell = hoteller[by];
+        let selHotell = document.createElement('select');
+        let s = "<option>.. velg hotell ..</option>";
+        for (let hotell of byHotell) {
+            s += `<option>${hotell}</option>`;
+        }
+        let divSkjema = document.createElement('div');
+        divSkjema.innerHTML = `
+        <form>
+        <br>Antall enkeltrom 
+          <input id="antall1" min="0" max="20" type="number"> <span></span>kr
+        <br>Antall dobbeltrom 
+          <input id="antall2" min="0" max="20" type="number"> <span></span>kr
+        <br>Bypass <input id="pass" type="checkbox">
+        <p><button type="button">Bestill</button></p>
+        </form>
+        <div>Totalt <span>.</span> kr</div>
+        `;
+        selHotell.innerHTML = s;
+        divByInfo.appendChild(divSkjema);
+        divByInfo.insertBefore(selHotell, divSkjema);
+        selHotell.addEventListener("change", hotellpris);
+        divSkjema.querySelector("button").addEventListener("click", visPris);
+
+        function hotellpris() {
+            let hotell = selHotell.value;
+            if (hotell.charAt(0) !== ".") {
+                // gyldig hotell
+                [enkelt, dobbelt] = prisliste[by][hotell];
+                divSkjema.querySelector("span:nth-of-type(1)").innerHTML = enkelt;
+                divSkjema.querySelector("span:nth-of-type(2)").innerHTML = dobbelt;
+                visPris();
+            }
+        }
+
+        function visPris() {
+            let antall2 = document.getElementById("antall2").valueAsNumber;
+            let antall1 = document.getElementById("antall1").valueAsNumber;
+            let pass = document.getElementById("pass").checked ? 1 : 0;
+            
+            if (antall1>0 && enkelt || antall2>0 && dobbelt) {
+                // nok info til å beregne pris
+                let pris = antall1 * enkelt + antall2 * dobbelt + 700 * pass;
+                divSkjema.querySelector("div > span").innerHTML = String(pris);
+            }
+        }
+
     }
 }
