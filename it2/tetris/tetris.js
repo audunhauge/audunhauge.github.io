e3ed4571b1cd368f3d4fcbb267bbf381640bbfbe
audun hauge <audun.hauge@gmail.com>
2018-03-19 12:07:44 +0100

    testing frame

--- it2/tetris/tetris.js
+++ it2/tetris/tetris.js
@@ -1,70 +1,79 @@
 // @flow
 
-import { Tetrino} from './Tetrino.js';
+import { Tetrino } from './Tetrino.js';
 import { Board } from './Board.js';
 
 function setup() {
   let divBrett = document.getElementById("brett");
   let divScore = document.getElementById("score");
-  let b = new Board(divBrett);
+  let board = new Board(divBrett);
   let tname = "T";
-  let t = new Tetrino(tname);
+  let tetrino = new Tetrino(tname);
   let score = 0;
+  let linesCleared = 0;
 
   let timer = setInterval(gameEngine, 800);
   document.addEventListener("keydown", respondToUser);
 
   function respondToUser(e) {
-    t.render(b, true); // wipe tetrino from board
+    tetrino.render(board, true); // wipe tetrino from board
     switch (e.keyCode) {
       case 40:
-        if (t.legalMove(b, 0, 0, 1)) t.rotate(1);
+        if (tetrino.legalMove(board, 0, 0, 1)) tetrino.rotate(1);
         break;
       case 38:
-        if (t.legalMove(b, 0, 0, -1)) t.rotate(-1);
+        if (tetrino.legalMove(board, 0, 0, -1)) tetrino.rotate(-1);
         break;
       case 37:
-        if (t.legalMove(b, -1, 0, 0)) t.x--;
+        if (tetrino.legalMove(board, -1, 0, 0)) tetrino.x--;
         break;
       case 39:
-        if (t.legalMove(b, 1, 0, 0)) t.x++;
+        if (tetrino.legalMove(board, 1, 0, 0)) tetrino.x++;
         break;
       case 32:
         clearInterval(timer);
         timer = setInterval(gameEngine, 30);
     }
-    t.render(b);  // render tetrino onto board
+    tetrino.render(board);  // render tetrino onto board
   }
 
   function gameEngine(e) {
-    if (t.legalMove(b, 0, 1, 0)) {
-      t.y++;
+    if (tetrino.legalMove(board, 0, 1, 0)) {
+      tetrino.y++;
     } else {
-      if (t.y < 0) {
+      if (tetrino.y < 0) {
         gameOver();
         clearInterval(timer);
         return;
       }
-      b.merge(t); // merge tetrino onto board
-      let lines = b.complete();
+      board.merge(tetrino); // merge tetrino onto board
+      let lines = board.complete();
       if (lines) {
-        score += 2 ** (lines - 1);
+        linesCleared++;
+        score += 3 ** (lines);
         divScore.innerHTML = String(score);
       }
       tname = "IOJLSZT"
         .replace(tname, "")
         .charAt(Math.floor(Math.random() * 6));
-      t = new Tetrino(tname);
+      tetrino = new Tetrino(tname);
       clearInterval(timer);
-      timer = setInterval(gameEngine, 800);
+      timer = setInterval(gameEngine, newSpeed(linesCleared));
     }
-    b.render();
-    t.render(b);
+    board.render();
+    tetrino.render(board);
   }
 
   function gameOver() {
-    document.getElementById("main").innerHTML = "Game over";
+    document.getElementById("main").innerHTML = `
+      <h1>Game over</h1>
+      You scored ${score} points and cleared ${linesCleared} lines
+    `
   }
 }
 
 setup();
+
+/** Pure functions */
+
+let newSpeed = (n) => Math.max(100,800 - 50*Math.floor(n/10));
