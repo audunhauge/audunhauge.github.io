8359a4976a0a65a00c13e5699ebce9b26a798c7f
audun hauge <audun.hauge@gmail.com>
2018-04-10 13:40:18 +0200

    Bedre Test.js

--- it2/lib/kode.js
+++ it2/lib/kode.js
@@ -18,10 +18,15 @@ function box(a) {
 
 expect(mult,3,4).to.be(12);
 expect(add,3,4).to.be(7);
+expect(add,3,4).it.is.eq(7);
 expect(box,12).to.have(0).eq(12);
 expect(box,12).to.have("length").eq(1);
-expect(box,12).to.have("length").defined;
+expect(box,12).it.to.have("length").defined;
 
 expect(add,1,2).to.looklike("3");
 expect(add,0.1,0.2).to.approx(0.3);
 expect(add,101,203).to.approx(300,10);
+
+expect({a:{bt:{c:13}}},"This should fail").to.have("a.b.c").eq(12);
+
+expect ({terminate:() => 12 }).to.have("terminate").it.is.a("function");
