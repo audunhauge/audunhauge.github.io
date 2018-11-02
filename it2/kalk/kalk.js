13ab59c3370a8036d3d68a49770da4f1d7f6eb3f
audunhauge <audun@SE02236.local>
2018-11-02 10:09:46 +0100

    ny kalk

--- it2/kalk/kalk.js
+++ it2/kalk/kalk.js
@@ -3,6 +3,7 @@
 function setup() {
     let divTallene = document.getElementById("tallene");
     let divOperatorer = document.getElementById("operatorer");
+    let divFunksjoner = document.getElementById("funksjoner");
     let inpDisplay = document.querySelector("#display > input");
 
     let nyttTall = true;
@@ -11,6 +12,31 @@ function setup() {
 
     divTallene.addEventListener("click", lesTall);
     divOperatorer.addEventListener("click", operer);
+    divFunksjoner.addEventListener("click", funger);
+
+    function funger(e) {
+        let t = e.target;
+        if (t.className === "fun") {
+            let id = t.id;
+            nyttTall = true;
+            switch (id) {
+                case "opErlik":
+                    beregn();
+                    break;
+                case "opAC":
+                    verdi = 0;
+                    inpDisplay.value = "0";
+                    operator = "";
+                    break;
+                case "opSin":
+                    verdi = Number(inpDisplay.value);
+                    let s = Math.sin(Math.PI * verdi / 180);
+                    inpDisplay.value = String(s);
+                    break;
+            }
+
+        }
+    }
 
     function lesTall(e) {
         let t = e.target;
@@ -27,12 +53,25 @@ function setup() {
     function beregn() {
         nyttTall = true;
         blink();
-        switch(operator) {
+        let update = true;
+        switch (operator) {
             case "+":
-              verdi += Number(inpDisplay.value);
-              inpDisplay.value = String(verdi);
-              break;
+                verdi += Number(inpDisplay.value);
+                break;
+            case "*":
+                verdi *= Number(inpDisplay.value);
+                break;
+            case "/":
+                verdi /= Number(inpDisplay.value);
+                break;
+            case "-":
+                verdi -= Number(inpDisplay.value);
+                break;
+            default:
+                update = false;
+                break;
         }
+        if (update) inpDisplay.value = String(verdi);
         verdi = Number(inpDisplay.value);
         operator = "";
     }
@@ -42,27 +81,27 @@ function setup() {
         if (t.className === "op") {
             beregn();
             let id = t.id;
-            switch(id) {
-                case "opC":
-                  break;
-                case "opAC":
-                  inpDisplay.value = "0";
-                  verdi = 0;
-                  operator = "";
-                  break;
+            switch (id) {
                 case "opPluss":
-                  operator = "+";
-                  break;
-                case "opErlik":
-                  break;
+                    operator = "+";
+                    break;
+                case "opMin":
+                    operator = "-";
+                    break;
+                case "opMul":
+                    operator = "*";
+                    break;
+                case "opDiv":
+                    operator = "/";
+                    break;
                 default:
-                  break;
+                    break;
             }
         }
     }
 
     function blink() {
         inpDisplay.classList.add("active");
-        setTimeout( () => inpDisplay.classList.remove("active"), 40);
+        setTimeout(() => inpDisplay.classList.remove("active"), 40);
     }
 }
