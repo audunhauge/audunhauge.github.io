b38827797c32a9799922f07ec3ec353a1cd6ac52
audunhauge <audun@SE02236.local>
2018-11-02 09:16:04 +0100

    kalkkk

--- it2/kalk/kalk.js
+++ it2/kalk/kalk.js
@@ -6,6 +6,8 @@ function setup() {
     let inpDisplay = document.querySelector("#display > input");
 
     let nyttTall = true;
+    let verdi = 0;
+    let operator = "";
 
     divTallene.addEventListener("click", lesTall);
     divOperatorer.addEventListener("click", operer);
@@ -16,22 +18,40 @@ function setup() {
             if (nyttTall) {
                 inpDisplay.value = "";
                 nyttTall = false;
+                blink();
             }
             inpDisplay.value += t.innerHTML;
         }
     }
 
+    function beregn() {
+        nyttTall = true;
+        blink();
+        switch(operator) {
+            case "+":
+              verdi += Number(inpDisplay.value);
+              inpDisplay.value = String(verdi);
+              break;
+        }
+        verdi = Number(inpDisplay.value);
+        operator = "";
+    }
+
     function operer(e) {
         let t = e.target;
         if (t.className === "op") {
+            beregn();
             let id = t.id;
             switch(id) {
                 case "opC":
                   break;
                 case "opAC":
-                  inpDisplay.value = "";
+                  inpDisplay.value = "0";
+                  verdi = 0;
+                  operator = "";
                   break;
                 case "opPluss":
+                  operator = "+";
                   break;
                 case "opErlik":
                   break;
@@ -39,5 +59,10 @@ function setup() {
                   break;
             }
         }
+    }
+
+    function blink() {
+        inpDisplay.classList.add("active");
+        setTimeout( () => inpDisplay.classList.remove("active"), 40);
     }
 }
