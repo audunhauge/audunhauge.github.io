8ef7367e8a6b140a2c8ba51a57df8c1c39a15f34
audunhauge <audun@SE02236.local>
2018-11-02 08:51:38 +0100

    kalk

--- it2/kalk/kalk.js
+++ it2/kalk/kalk.js
@@ -4,82 +4,40 @@ function setup() {
     let divTallene = document.getElementById("tallene");
     let divOperatorer = document.getElementById("operatorer");
     let inpDisplay = document.querySelector("#display > input");
-    divTallene.addEventListener("click", tallTast);
-    divOperatorer.addEventListener("click", operatorTast);
 
-    let nyStart = true;
-    let verdi = 0n;          // verdien vi har beregna så langt
-    let operand = "";       // handlingen vi skal gjøre
+    let nyttTall = true;
 
-    function tallTast(e) {
+    divTallene.addEventListener("click", lesTall);
+    divOperatorer.addEventListener("click", operer);
+
+    function lesTall(e) {
         let t = e.target;
         if (t.className === "tall") {
-            let display = inpDisplay.value;
-            if (nyStart) {
-                display = "";
-                blinkDisplay();
-                nyStart = false;
+            if (nyttTall) {
+                inpDisplay.value = "";
+                nyttTall = false;
             }
-            display += t.innerHTML;
-            inpDisplay.value = display;
+            inpDisplay.value += t.innerHTML;
         }
     }
 
-    function operatorTast(e) {
+    function operer(e) {
         let t = e.target;
         if (t.className === "op") {
-            let opid = t.id;
-            // rettetasten er spesiell:
-            //   alle andre kan kjøre beregn() først,
-            //   men rettetasten skal bare fjerne siste verdi
-            //   dersom nyStart ikke er true
-            if (opid === "opC" && ! nyStart) {
-                let display = inpDisplay.value;
-                let l = display.length;
-                inpDisplay.value = display.substr(0,l-1);
-                return;
+            let id = t.id;
+            switch(id) {
+                case "opC":
+                  break;
+                case "opAC":
+                  inpDisplay.value = "";
+                  break;
+                case "opPluss":
+                  break;
+                case "opErlik":
+                  break;
+                default:
+                  break;
             }
-            // alle andre operator taster
-            beregn();
-            switch (opid) {
-                case 'opAC':
-                    inpDisplay.value = "0.0";
-                    verdi = 0n;
-                    break;
-                case 'opPluss':
-                    operand = "pluss";
-                   
-            }
-
         }
-    }
-
-    function beregn() {
-        blinkDisplay();
-        switch(operand) {
-            case "pluss":
-              verdi += BigInt(inpDisplay.value);
-              inpDisplay.value = String(verdi);
-              break;
-            default:
-              // merk at ErLik faller ned her
-              verdi = BigInt(inpDisplay.value);
-        }
-        // operand = "";
-        
-        // dersom denne kommenteres ut kan en utføre siste operasjon 
-        // på nytt med ErLik knappen - 2+2= 4, = 8 = 16 = 32 ...
-        // = vil nå legge sammen siste verdi med den som er i display, disse er like
-        // så vi får en dobling hver gang
-
-        nyStart = true;
-    }
-
-    /**
-     * Visuell markering av at vi starter på nytt Tall
-     */
-    function blinkDisplay() {
-        inpDisplay.classList.add("active");
-        setTimeout(() => inpDisplay.classList.remove("active"),100);
     }
 }
