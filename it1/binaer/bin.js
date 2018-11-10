bf57d9de9b049b47fdb14e26e8563476d5098cef
audunhauge <audun.hauge@gmail.com>
2018-11-10 14:20:05 +0100

    Can change decimal by clicking

--- it1/binaer/bin.js
+++ it1/binaer/bin.js
@@ -5,6 +5,7 @@ function setup() {
     let divVerdi = document.getElementById("verdi");
 
     divRegister.addEventListener("click", beregn);
+    divVerdi.addEventListener("click", visBin);
 
     function beregn(e) {
         let t = e.target;
@@ -15,6 +16,19 @@ function setup() {
         let sum = bits.map(e => +e.dataset.tall).reduce((s,v) => s+v,0);
         divVerdi.innerHTML = String(sum);
 
+    }
+    
+    function visBin(e) {
+        let v = Number(divVerdi.innerHTML);
+        v++;
+        divVerdi.innerHTML = String(v);
+        let bits = Array.from(divRegister.querySelectorAll(".bit"));
+        bits.forEach(b => {
+            b.classList.remove("on");
+            if (Number(b.dataset.tall) & v) {
+                b.classList.add("on");
+            }
+        });
     }
 
 }
