95aa8f2effdf1d5190ce9c6418be2074a296a336
audunhauge <audun@SE02236.local>
2018-12-07 08:34:02 +0100

    new pix

--- it1/julekal/jul.js
+++ it1/julekal/jul.js
@@ -4,12 +4,32 @@ function setup() {
     let divVis = document.getElementById("vis");
     let lukene = Array.from(document.querySelectorAll(".luke"));
     lukene.forEach( e => e.addEventListener("click", visLuke));
+
+    let soundHappy = document.getElementById("happy");
+    let soundSad = document.getElementById("sad");
+
+    soundHappy.src = "media/happy.mp3";
+    soundSad.src = "media/happy.mp3";
+    
+
+
     
     function visLuke(e) {
+      let today = new Date();
       let t = e.target;
       let nr = Number(t.innerHTML);
-      divVis.style.backgroundImage = `url("bilde${nr}.png")`;
+      if (nr > today.getDate()) {
+        spill(soundSad);
+        return;
+      }
+      spill(soundHappy);
+      divVis.style.backgroundImage = `url("media/bilde${nr}.png")`;
       divVis.style.display = "block";
       setTimeout(() =>  divVis.style.display = "none", 2000);
     }
+  }
+
+
+  function spill(sound) {
+    sound.play();
   }
