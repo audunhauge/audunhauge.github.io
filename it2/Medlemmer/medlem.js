6f17cb1f8a27731899b77523aea43714e45a5f38
Audun Hauge <audun.hauge@gmail.com>
2019-09-09 10:33:18 +0200

    registrering

--- it2/Medlemmer/medlem.js
+++ it2/Medlemmer/medlem.js
@@ -1,5 +1,20 @@
 // @ts-check
 
+class Medlem {
+    epost;
+    fornavn;
+    etternavn;
+    alder;
+    mobil;
+    constructor(epost, fn, en, alder, mob) {
+        this.epost = epost;
+        this.fornavn = fn;
+        this.etternavn = en;
+        this.alder = alder;
+        this.mobil = mob;
+    }
+}
+
 function $(id) {
     return document.getElementById(id);
 }
@@ -11,17 +26,46 @@ function setup() {
     let inpAlder = $("alder");
     let inpMobil = $("mobil");
     let btnRegistrer = $("registrer");
+    let divListe = $("liste");
+    let btnVis = $("vis");
 
     btnRegistrer.addEventListener("click", registrer);
+    btnVis.addEventListener("click", visListe);
+
+
+    let medlemsListe = [ ];
+
+    function visListe() {
+        visMedlemmer(medlemsListe, divListe);
+    }
 
     function registrer() {
         // @ts-ignore
         let epost = inpEpost.value;
         // @ts-ignore
         let fornavn = inpFornavn.value;
-        console.log(epost, fornavn);
+        // @ts-ignore
+        let etternavn = inpEtternavn.value;
+        // @ts-ignore
+        let alder = inpAlder.valueAsNumber || 20;
+        // @ts-ignore
+        let mobil = inpMobil.value;
+
+        let medlem = new Medlem(epost,fornavn,etternavn,alder,mobil);
+
+        medlemsListe.push(medlem);
+
     }
+}
 
-    
-
+function visMedlemmer(arr, div) {
+    let s = "<table>";
+    for (let i=0; i< arr.length; i += 1) {
+        let m = arr[i];
+        // s += m.fornavn + " " + m.etternavn  + "<br>";
+        s += `<tr><td>${m.fornavn}</td><td>${m.etternavn}</td><td>${m.alder}
+                  </td><td>${m.epost}</td><td>${m.mobil}</td></tr>`;
+    }
+    s += "</table>"
+    div.innerHTML = s;
 }
