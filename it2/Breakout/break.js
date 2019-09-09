2c26a1c2e1cc4726dd31c15c19d7db04caf7b0df
Audun Hauge <audun.hauge@gmail.com>
2019-09-09 13:57:40 +0200

    better class

--- it2/Breakout/break.js
+++ it2/Breakout/break.js
@@ -1,5 +1,40 @@
 // @ts-check
 
+class Sprite {
+    div;
+    x;
+    y;
+    w;
+    h;
+    constructor(div,x,y,w,h) {
+        this.div = div;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+
+    draw() {
+        this.div.style.top = this.y + "px";
+        this.div.style.left = this.x + "px";
+    }
+}
+
+class Movable extends Sprite {
+    vx;
+    vy;
+    constructor(div,x,y,w,h,vx,vy) {
+        super(div,x,y,w,h);
+        this.vx = vx;
+        this.vy = vy;
+    }
+
+    move() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+}
+
 function $(element) {
     return document.getElementById(element);
 }
@@ -8,22 +43,32 @@ function setup() {
     let divBrett = $("brett");
     let divInfo = $("info");
     let divScore = $("score");
+    let ball, plate;
 
-    lagBrikker();
-    lagPlate();
-    lagBall();
-    startSpill();
+    const keys = new Set();
 
-    function lagPlate() {
-        let div = document.createElement("div");
-        div.className = "plate";
-        divBrett.appendChild(div);
+    function saveKey(e) {
+        let k = e.key;
+        keys.add(k);
+    }
+
+    function dropKey(e) {
+        let k = e.key;
+        keys.delete(k);
     }
 
     function lagBall() {
         let div = document.createElement("div");
         div.className = "ball";
         divBrett.appendChild(div);
+        ball = new Movable(div, 100, 400,10,10,2,2);
+    }
+
+    function lagPlate() {
+        let div = document.createElement("div");
+        div.className = "plate";
+        divBrett.appendChild(div);
+        plate = new Movable(div,100,450,150,10,0,0)
     }
 
     function lagBrikker() {
@@ -38,6 +83,32 @@ function setup() {
         }
     }
 
-    function startSpill() {
+    function animate() {
+        plate.vx *= 0.95;
+        if (keys.has("s")) {
+            plate.vx = 5;
+        }
+        if (keys.has("a")) {
+            plate.vx = -5;
+        }
+        ball.move();
+        plate.move();
+        ball.draw();
+        plate.draw();
+
     }
+
+    lagBrikker();
+    lagPlate();
+    lagBall();
+    startSpill();
+
+    function startSpill() {
+        addEventListener("keydown", saveKey);
+        addEventListener("keyup", dropKey);
+        setInterval(animate, 50);
+    }
+
+   
+
 }
