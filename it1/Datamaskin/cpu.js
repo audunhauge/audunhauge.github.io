75d8e0aa690377b8dcbd5ad3c37114a946377e0f
Audun Hauge <audun.hauge@gmail.com>
2019-09-26 15:16:15 +0200

    cpu

---
+++ it1/Datamaskin/cpu.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+function setup() {
+    let skisse = document.getElementById("skisse");
+    skisse.addEventListener("click", showme);
+
+    function showme(e) {
+        let u = e.target;
+        if (u.dataset && u.dataset.more) {
+            location.href = u.dataset.more + "html";
+        }
+
+    }
+
+}
