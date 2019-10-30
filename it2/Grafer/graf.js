fada4562a511bcb0e3be9e1be261ebf3e794d6a7
Audun Hauge <audun.hauge@gmail.com>
2019-10-30 09:19:01 +0100

    graftegner

--- it2/Grafer/graf.js
+++ it2/Grafer/graf.js
@@ -1,67 +1,89 @@
 // @ts-check
 
-const temperaturData = [];
+const temperaturListe = [];
 
 function setup() {
-    let divMain = document.getElementById("main");
-    let cnvBilde = document.getElementById("bilde");
-    let inpTemp = document.getElementById("temp");
-    let btnTegn = document.getElementById("tegn");
+    let inpTemp = document.getElementById('temp');
+    let canvas = document.getElementById('bilde');
+    let ctx = canvas.getContext('2d');
+    inpTemp.focus();
 
-    addEventListener("keydown", sjekkInput);
-
-    btnTegn.addEventListener("click", tegnGraf);
-    const ctx = cnvBilde.getContext('2d');
-
-
-
+    inpTemp.addEventListener("keypress", sjekkInput);
 
     function sjekkInput(e) {
         if (e.key === "Enter") {
             let temp = Number(inpTemp.value);
             if (Number.isFinite(temp)) {
-                temperaturData.push(temp);
+                temperaturListe.push(temp);
                 inpTemp.value = "";
                 inpTemp.focus();
+                tegnGraf();
             }
         }
-    }
-
-    function sirkel(x,y) {
-        
     }
 
     function tegnGraf() {
-        
         ctx.clearRect(0, 0, 500, 500);
         ctx.beginPath();
+        ctx.font = '16px serif';
+        ctx.fillStyle = 'green';
         ctx.strokeStyle = "blue";
-        if (temperaturData.length > 1) {
-            let diff = 480 / (temperaturData.length - 1)
-            let temp = temperaturData[0];
-            let y = 500 - (temp + 20) * 500 / 60
+        let antall = temperaturListe.length;
+        if (antall > 1) {
+            let temp = temperaturListe[0];
+            let diff = 480 / (antall - 1);
+            let y = 500 - (temp + 20) * 500 / 60;
             ctx.moveTo(10, y);
-            for (let i = 1; i < temperaturData.length; i++) {
-                let temp = temperaturData[i];
-                let y = 500 - (temp + 20) * 500 / 60
-                ctx.lineTo(10 + diff * i, y);
+            for (let i = 1; i < antall; i++) {
+                let temp = temperaturListe[i];
+                let y = 500 - (temp + 20) * 500 / 60;
+                ctx.lineTo(10 + i * diff, y);
             }
             ctx.stroke();
+            ctx.beginPath();
+            ctx.strokeStyle = "green";
+            ctx.fillStyle = 'white';
+            for (let i = 0; i < antall; i++) {
+                let temp = temperaturListe[i];
+                let y = 500 - (temp + 20) * 500 / 60;
+                let x = 10 + i * diff;
+                ctx.moveTo(x, y);
+                ctx.arc(x, y, 3, 0, Math.PI * 2, true);
+            }
+            ctx.stroke();
+            ctx.fill();
+            ctx.beginPath();
+            ctx.fillStyle = 'red';
+            for (let i = 0; i < antall; i++) {
+                let temp = temperaturListe[i];
+                let y = 500 - (temp + 20) * 500 / 60;
+                let x = 10 + i * diff;
+                if (x > 460) {
+                    ctx.fillText(String(temp), x - 20, y);
+                } else {
+                    ctx.fillText(String(temp), x, y);
+                }
+            }
+            ctx.fill();
+        } else {
+            ctx.fillRect(10, 10, 30, 30);
         }
     }
-    /*
-    ctx.fillStyle = 'green';
-    ctx.fillRect(100, 10, 15, 10);
-    ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
-    ctx.fill();
-    ctx.fillStyle = 'red';
-    ctx.beginPath();
-    ctx.arc(175, 75, 20, 0, Math.PI * 2, true); // Outer circle
-    ctx.fill();
-    */
+
 
 }
+
+/*
+ctx.fillStyle = 'green';
+ctx.fillRect(100, 10, 15, 10);
+ctx.fillStyle = 'blue';
+ctx.beginPath();
+ctx.moveTo(75, 50);
+ctx.lineTo(100, 75);
+ctx.lineTo(100, 25);
+ctx.fill();
+ctx.fillStyle = 'red';
+ctx.beginPath();
+ctx.arc(175, 75, 20, 0, Math.PI * 2, true); // Outer circle
+ctx.fill();
+*/
