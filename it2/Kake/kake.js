0a9fc6be2fc551b8ea8cb3bd156bfe04e91df6a3
Audun Hauge <audun.hauge@gmail.com>
2019-11-04 13:33:46 +0100

    kake dia virker

--- it2/Kake/kake.js
+++ it2/Kake/kake.js
@@ -2,15 +2,16 @@
 
 
 class Matvare {
-    constructor(navn,kcal) {
+    constructor(navn, kcal) {
         this.navn = navn;
         this.kcal = kcal;
     }
 }
 
-const matvareListe = [ ];
+const matvareListe = [];
 
-const farger = "red,green,blue,yellow,orange,teal,pink".split(",");
+const farger = "red,green,blue,yellow,orange,teal,gray,white,pink".split(",");
+const fargtall = farger.length;
 
 
 function setup() {
@@ -26,22 +27,50 @@ function setup() {
             let navn = inpMat.value;
             let kcal = Number(inpKcal.value);
             if (navn.length > 0 && kcal > 0) {
-                let matvare = new Matvare(navn,kcal);
+                let matvare = new Matvare(navn, kcal);
                 matvareListe.push(matvare);
                 tegnKakeDiagram();
+                inpKcal.value = "";
+                inpMat.value = "";
+                inpMat.focus();
+                return;
             }
+            if (e.target.id === "mat") {
+                // trykket enter i mat - flytt til kcal
+                // slipper å bruke tab
+                inpKcal.focus();
+            }
+
         }
     }
 
-    function tegnKakeDiagram() {
-        let fargeIndex = 0;
-        let farge = farger[fargeIndex];
-        ctx.beginPath();
-        ctx.fillStyle = farge;
-        ctx.clearRect(0,0,500,500);
-        //ctx.fillRect(10, 10, 30, 30);
-        ctx.arc(250, 250, 230, 0, Math.PI * 2, true);
-        ctx.fill();
+    function totalSum(arr) {
+        let sum = 0;
+        for (let matvare of arr) {
+            sum += matvare.kcal;
+        }
+        return sum;
+    }
 
+    function tegnKakeDiagram() {
+        let total = totalSum(matvareListe);
+        let start = 0;
+        let fargeIndex = 0;
+        ctx.clearRect(0, 0, 500, 500);
+        for (let matvare of matvareListe) {
+            ctx.beginPath();
+            ctx.moveTo(250, 250);
+            let farge = farger[fargeIndex % fargtall];
+            ctx.fillStyle = farge;
+            let grader = Math.PI * 2 * matvare.kcal / total;
+            ctx.arc(250, 250, 230, start, start + grader, false);
+            ctx.fill();
+            fargeIndex++;
+            start += grader;
+        }
+        // tegn en sirkel rundt kaka
+        ctx.beginPath();
+        ctx.arc(250, 250, 230, 0, 2 * Math.PI, false);
+        ctx.stroke();
     }
 }
