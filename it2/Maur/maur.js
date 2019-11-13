4984de4a347df47a131adabfbf760fe562d56898
Audun Hauge <audun.hauge@gmail.com>
2019-11-13 09:19:47 +0100

    dead ants

--- it2/Maur/maur.js
+++ it2/Maur/maur.js
@@ -12,23 +12,40 @@ class Keys {
 }
 
 class Sprite {
-    constructor(div,x,y) {
+    constructor(div, x, y, w, h) {
         this.div = div;
         this.x = x;
         this.y = y;
+        this.w = w;
+        this.h = h;
         this.r = 0;
     }
     render() {
         this.div.style.transform = `translate(${this.x}px,${this.y}px) rotate(${this.r}rad)`;
     }
+
+    overlap(andre) {
+        let a = this;
+        let b = andre;
+        return a.x > b.x - a.w &&
+            a.x < b.x + b.w &&
+            a.y > b.y - a.h &&
+            a.y < b.y + b.h;
+    }
 }
 
 class Maur extends Sprite {
-    constructor(div, x, y, v) {
-        super(div,x,y);
+    constructor(div, x, y, w, h, v) {
+        super(div, x, y, w, h);
         this.v = v;
         this.turn = Math.random() - 0.5;
         this.r = Math.random() * Math.PI * 2;
+        this.alive = true;
+    }
+
+    die() {
+        this.alive = false;
+        this.div.style.opacity = 0;
     }
 
     flytt() {
@@ -40,7 +57,7 @@ class Maur extends Sprite {
         if (this.x < 0) vx = Math.abs(vx);
         if (this.y > 500) vy = -Math.abs(vy);
         if (this.y < 0) vy = Math.abs(vy);
-        this.r = Math.atan2(vy,vx);
+        this.r = Math.atan2(vy, vx);
         this.r += this.turn * 0.1;
         if (Math.random() > 0.9) {
             this.turn = Math.random() - 0.5;
@@ -49,8 +66,8 @@ class Maur extends Sprite {
 }
 
 class Finger extends Sprite {
-    constructor(div,x,y) {
-        super(div,x,y);
+    constructor(div, x, y, w, h) {
+        super(div, x, y);
     }
 }
 
@@ -62,7 +79,7 @@ function setup() {
     let divGame = document.getElementById("game");
     let divFing = document.getElementById("fing");
 
-    let fing = new Finger(divFing,250,250);
+    let fing = new Finger(divFing, 250, 250, 40, 40);
 
     setInterval(animate, 40);
 
@@ -74,15 +91,29 @@ function setup() {
             let x = Math.random() * 500;
             let y = Math.random() * 500;
             let v = 2 + Math.random() * 5;
-            let m = new Maur(div,x,y,v);
+            let m = new Maur(div, x, y, 20, 5, v);
             alleMaur.push(m);
         }
         for (let maur of alleMaur) {
-            maur.flytt();
-            maur.render();
+            if (maur.alive) {
+                maur.flytt();
+                maur.render();
+                if (maur.overlap(fing)) {
+                    maur.die();
+                }
+            }
         }
         if (Keys.has("ArrowDown")) {
-            fing.y += 10; 
+            fing.y += 10;
+        }
+        if (Keys.has("ArrowUp")) {
+            fing.y -= 10;
+        }
+        if (Keys.has("ArrowLeft")) {
+            fing.x -= 10;
+        }
+        if (Keys.has("ArrowRight")) {
+            fing.x += 10;
         }
         fing.render();
     }
