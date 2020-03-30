915f34742080f71d19feb5ea9bade4c606d3a43c
audun hauge <audun.hauge@gmail.com>
2020-03-30 11:18:39 +0200

    classes

--- it2/sketch/sketch.js
+++ it2/sketch/sketch.js
@@ -1,17 +1,59 @@
 // @ts-check
 
+class Point {
+    constructor({x,y}) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Vector extends Point {
+    constructor({x,y}) {
+        super({x,y});
+    }
+    add(v) {
+        this.x += v.x;
+        this.y += v.y;
+    }
+}
+
+class Square extends Point {
+    constructor({x,y,w,h}) {
+        super({x,y});
+        this.w = w;
+        this.h = h;
+    }
+    render() {
+        ctx.beginPath();
+        ctx.strokeStyle = farge;
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+    }
+}
+
+let canCanvas;
+let ctx,farge;
 const g = id => document.getElementById(id);
+
 
 function setup() {
   const divTools = g("tools");
-  const canCanvas = g("canvas");
+  canCanvas = g("canvas");
   const divFarger = g("farger");
+  ctx = canCanvas.getContext("2d");
   // @ts-ignore
-  const ctx = canCanvas.getContext('2d');
+  
 
-  let farge = "blue";
+  farge = "blue";
 
   divTools.addEventListener("click", activateTool);
+  divFarger.addEventListener("click", chooseColor);
+
+  function chooseColor(e) {
+    const t = e.target;
+    if (t.title) { 
+        farge = t.title;
+    } 
+  }
 
   function activateTool(e) {
     const t = e.target;
@@ -20,24 +62,30 @@ function setup() {
         case "peker":
           break;
         case "firkant":
-          tegnFirkant();
+          let f = new Square({x:200,y:200,w:50,h:50})
+          f.render();
           break;
         case "sirkel":
+          tegnSirkel();
           break;
         case "trekant":
           break;
         case "strek":
           break;
         case "visk":
+          ctx.clearRect(0, 0, 500, 500);
           break;
       }
     }
   }
 
-  function tegnFirkant() {
-    ctx.clearRect(0, 0, 500, 500);
+
+  function tegnSirkel() {
     ctx.beginPath();
     ctx.strokeStyle = farge;
-    ctx.strokeRect(200, 200,50,50);
+    ctx.arc(200, 200, 50, 0, 2 * Math.PI, false);
+    ctx.stroke();
   }
+
+  
 }
